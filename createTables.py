3e344f073ab30e6cd7131a42fe873ce0bea5ada5
pcloud <pcloud@myiac.mymdc.net>
2019-10-04 14:30:24 -0400

    template

---
+++ createTables.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+try:
+    # required information to use the db 
+    conn = psycopg2.connect(
+        database = "investor", # database name
+        user = "postgres",
+        password = "6108", #your password
+        host = "127.0.0.1",
+        port = "5432"
+    )
+    
+    def createTables(): # function to create the tables
+        cursor = conn.cursor()
+        
+        # create houses table
+        cursor.execute(
+            """CREATE TABLE houses (
+			house_id int primary key NOT NULL,
+			house_tag text NOT NULL,
+			house_selling_price int NOT NULL,
+            house_purchase_price int NOT NULL,
+            house_spread int NOT NULL)"""
+		)
+        conn.commit()
+        
+    createTables()
+        
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while fetching data PostgreSQL", error)
